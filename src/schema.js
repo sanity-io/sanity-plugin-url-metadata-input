f5e18e12ac554c65c42928cafa1b5109a1cc26ef
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-09-01 11:39:22 +0200

    Initial version

---
+++ src/schema.js
@@ -0,0 +1,170 @@
+export default {
+  name: 'urlWithMetadata',
+  type: 'object',
+  fieldsets: [
+    {
+      name: 'meta',
+      title: 'Link metadata',
+      options: {collapsable: true}
+    }
+  ],
+  fields: [
+    {
+      title: 'URL',
+      name: 'url',
+      type: 'url'
+    },
+    {
+      title: 'Resolved URL',
+      name: 'resolvedUrl',
+      type: 'url'
+    },
+    {
+      title: 'Crawled at',
+      name: 'crawledAt',
+      type: 'string' // @todo change to date when dates are... simple dates
+    },
+
+    {
+      name: 'meta',
+      title: 'Metadata',
+      fieldset: 'meta',
+      type: 'object',
+      fields: [
+        {
+          title: 'Title',
+          name: 'title',
+          type: 'string'
+        },
+        {
+          title: 'Description',
+          name: 'description',
+          type: 'string'
+        }
+      ]
+    },
+    {
+      name: 'openGraph',
+      title: 'OpenGraph',
+      fieldset: 'meta',
+      type: 'object',
+      fields: [
+        // Basic metadata
+        {
+          title: 'Title',
+          name: 'title',
+          type: 'string',
+        },
+        {
+          title: 'Description',
+          name: 'description',
+          type: 'string',
+        },
+        {
+          title: 'Site name',
+          name: 'siteName',
+          type: 'string',
+        },
+        {
+          title: 'Type',
+          name: 'type',
+          type: 'string',
+        },
+        {
+          title: 'URL',
+          name: 'url',
+          type: 'url',
+        },
+
+        // Image
+        {
+          title: 'Image URL',
+          name: 'image',
+          type: 'string',
+        },
+        {
+          title: 'Image description',
+          name: 'imageAlt',
+          type: 'string',
+        },
+        {
+          title: 'Image Secure URL',
+          name: 'imageSecureUrl',
+          type: 'url',
+        },
+        {
+          title: 'Image type',
+          name: 'imageType',
+          type: 'string',
+        },
+        {
+          title: 'Image height',
+          name: 'imageHeight',
+          type: 'number',
+        },
+        {
+          title: 'Image width',
+          name: 'imageWidth',
+          type: 'number',
+        },
+
+        // Audio
+        {
+          title: 'Audio URL',
+          name: 'audio',
+          type: 'url',
+        },
+        {
+          title: 'Audio Secure URL',
+          name: 'audioSecureUrl',
+          type: 'url',
+        },
+        {
+          title: 'Audio type',
+          name: 'audioType',
+          type: 'string',
+        },
+        {
+          title: 'Determiner',
+          name: 'determiner',
+          type: 'string',
+          options: {
+            list: ['', 'a', 'an', 'the', 'auto'].map(value => ({value, title: value}))
+          }
+        },
+        {
+          title: 'Locale',
+          name: 'locale',
+          type: 'string',
+        },
+
+        // Video
+        {
+          title: 'Video URL',
+          name: 'video',
+          type: 'url',
+        },
+        {
+          title: 'Video Secure URL',
+          name: 'videoSecureUrl',
+          type: 'url',
+        },
+        {
+          title: 'Video type',
+          name: 'videoType',
+          type: 'string',
+        },
+        {
+          title: 'Video height',
+          name: 'videoHeight',
+          type: 'number',
+        },
+        {
+          title: 'Video width',
+          name: 'videoWidth',
+          type: 'number',
+        }
+      ]
+    }
+  ]
+}
